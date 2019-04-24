aab8b3d3816f6f2ddd9a0b858bd93f9a5aa84259
Somad Yessoufou <somad.yessoufou@persol.net>
2019-04-24 08:19:42 +0100

    Serialized Exceptions as Json

---
+++ LandingAPI/Models/ApiError.cs
@@ -0,0 +1,8 @@
+﻿namespace LandingAPI.Models
+{
+    public class ApiError
+    {
+        public string Message { get; set; }
+        public string Detail { get; set; }
+    }
+}
--- LandingAPI/Startup.cs
+++ LandingAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LandingAPI.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,7 +28,12 @@ namespace LandingAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvc(opt => 
+                {
+                    opt.Filters.Add<JsonExceptionFilter>();
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // Register the Swagger services
             services.AddSwaggerDocument();
