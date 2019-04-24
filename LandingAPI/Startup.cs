1989e438671617e3a6e006af0a2b2db6380f51ab
Somad Yessoufou <somad.yessoufou@persol.net>
2019-04-24 06:31:23 +0100

    New API Project

--- LandingAPI/Startup.cs
+++ LandingAPI/Startup.cs
@@ -26,6 +26,8 @@ namespace LandingAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddRouting(opt => opt.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
